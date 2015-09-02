2b91b71b19eab9bf9081d1723ccbeba44235016d
RachelPisciotta <rpisciot@gmail.com>
2015-09-02 00:02:09 -0400

    first edit of first php

--- first.php
+++ first.php
@@ -2,6 +2,18 @@
 <body>
 <?php
 echo "Hello world!";
+
+$fn ="Rachel";
+$ln ="Pisciotta";
+$mn = "Lorraine";
+
+$age =27;
+
+echo "$ln, $fn $mn ($age)";
+
+echo "<ul><li>$ln, $fn $mn ($age)</li></ul>";
+
+
 ?>
 </body>
 </html>
